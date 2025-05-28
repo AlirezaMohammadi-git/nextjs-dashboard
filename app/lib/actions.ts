cae35a61a807e793bef9becbe42e7a20bc0f0c61
Joe <a.m.sadra4@gmail.com>
2025-05-28 08:59:53 +0330

    updating accessability for update form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -71,17 +71,33 @@ export async function createInvoice(prevState: State, formData: FormData) {
     redirect('/dashboard/invoices')
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = CreateOrUpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+    const validatedFields = CreateOrUpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
-    await sql`
+
+    try {
+        await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents} , status = ${status}
     WHERE id = ${id}`
+    } catch (err) {
+        return {
+            error: "Database Error : Failed to update invoice!"
+        }
+    }
 
     // triggering a new request to server and clearing cache.
     // because data displayed in invoices route has been updated.
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,8 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import { updateInvoice } from '@/app/lib/actions';
+import { State, updateInvoice } from '@/app/lib/actions';
+import { useActionState } from 'react';
 
 export default function EditInvoiceForm({
   invoice,
@@ -19,11 +20,14 @@ export default function EditInvoiceForm({
   customers: CustomerField[];
 }) {
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id)
+  const initialState: State = { message: null, errors: {} };
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState)
+
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
-        <div className="mb-4">
+        <div className="mb-4" aria-describedby='customerId'>
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
             Choose customer
           </label>
@@ -45,6 +49,11 @@ export default function EditInvoiceForm({
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
+
+          {
+            state.errors?.customerId && <p className='text-red-500'>{state.errors?.customerId}</p>
+          }
+
         </div>
 
         {/* Invoice Amount */}
@@ -66,6 +75,9 @@ export default function EditInvoiceForm({
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
+          {
+            state.errors?.amount && <p className='text-red-500'>{state.errors.amount}</p>
+          }
         </div>
 
         {/* Invoice Status */}
